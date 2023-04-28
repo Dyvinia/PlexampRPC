ce274fad430e15be6629a8a98d5cbf9c6683a71d
Dyvine57 <dulana57@gmail.com>
2023-04-28 00:50:07 -0400

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,119 @@
+﻿using DiscordRPC;
+using DiscordRPC.Logging;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Plex.ServerApi;
+using Plex.ServerApi.Api;
+using Plex.ServerApi.Clients;
+using Plex.ServerApi.Clients.Interfaces;
+using Plex.ServerApi.PlexModels.Account;
+using Plex.ServerApi.PlexModels.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security.Policy;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.TextFormatting;
+using System.Windows.Threading;
+
+namespace PlexampRPC {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+
+        public static IPlexAccountClient AccountClient = new PlexAccountClient(new() {
+            Product = "PlexampRPC",
+            DeviceName = Environment.MachineName,
+            Platform = "Desktop",
+            Version = "v2"
+        }, new ApiService(new PlexRequestsHttpClient(), new Logger<ApiService>(new NullLoggerFactory())));
+
+        public static IPlexServerClient ServerClient = new PlexServerClient(new() {
+            Product = "PlexampRPC",
+            DeviceName = Environment.MachineName,
+            Platform = "Desktop",
+            Version = "v2"
+        }, new ApiService(new PlexRequestsHttpClient(), new Logger<ApiService>(new NullLoggerFactory())));
+
+
+        public static string Token { get; set; }
+        public static PlexAccount? Account { get; set; }
+        public static AccountServerContainer? ServerContainer { get; set; }
+        public static DiscordRpcClient DiscordClient { get; set; }
+
+        public App() {
+
+            Environment.CurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            DispatcherUnhandledException += Application_DispatcherUnhandledException;
+        }
+
+        protected override async void OnStartup(StartupEventArgs e) {
+            MainWindow window = new();
+            window.Show();
+
+            InitRPC();
+            await PlexSignIn();
+
+            window.WindowState = WindowState.Normal;
+            window.Activate();
+            window.GetAccountInfo();
+            window.StartPolling();
+
+            
+        }
+
+        private async Task PlexSignIn(bool resignIn = false) {
+            string authFile = "auth.txt";
+            if (File.Exists(authFile) && !resignIn) {
+                Token = File.ReadAllText(authFile);
+            }
+            else {
+                Token = await PlexOAuth();
+                File.WriteAllText(authFile, Token);
+            }
+            Account = await AccountClient.GetPlexAccountAsync(Token);
+            ServerContainer = await AccountClient.GetAccountServersAsync(Token);
+        }
+
+        private async Task<string> PlexOAuth() {
+            OAuthPin plexPin;
+            OAuthPin? oauthUrl = await AccountClient.CreateOAuthPinAsync("");
+
+            Process.Start(new ProcessStartInfo(oauthUrl.Url) { UseShellExecute = true });
+
+            while (true) {
+                plexPin = await AccountClient.GetAuthTokenFromOAuthPinAsync(oauthUrl.Id.ToString());
+                if (!string.IsNullOrEmpty(plexPin.AuthToken)) break;
+                await Task.Delay(1000);
+            }
+            return plexPin.AuthToken;
+        }
+
+        private void InitRPC() {
+            DiscordClient = new("1100233636491563069") {
+                Logger = new ConsoleLogger() { Level = DiscordRPC.Logging.LogLevel.Warning }
+            };
+
+            DiscordClient.OnReady += (sender, e) => {
+                Console.WriteLine($"Received Ready from user {e.User.Username}");
+            };
+
+            DiscordClient.OnPresenceUpdate += (sender, e) => {
+                Console.WriteLine($"Received Update! {e.Presence}");
+            };
+            DiscordClient.Initialize();
+        }
+
+        private void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e) {
+            e.Handled = true;
+            string title = "";
+            //ExceptionDialog.Show(e.Exception, title, true);
+        }
+    }
+}
---
+++ Windows/MainWindow.xaml.cs
@@ -0,0 +1,192 @@
+﻿using DiscordRPC;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Plex.Api.Factories;
+using Plex.Library.Factories;
+using Plex.ServerApi.Clients;
+using Plex.ServerApi.Clients.Interfaces;
+using Plex.ServerApi.PlexModels.Account;
+using Plex.ServerApi.PlexModels.Audio;
+using Plex.ServerApi.PlexModels.Server.Activities;
+using Plex.ServerApi.PlexModels.Server.Sessions;
+using System;
+using System.Buffers.Text;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace PlexampRPC {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+
+        public static SessionMetadata? Session { get; set; }
+
+        public class PresenceData {
+            public string Title { get; set; }
+            public string Artist { get; set; }
+            public string Album { get; set; }
+            public string ArtLink { get; set; }
+            public string State { get; set; }
+            public int TimeOffset { get; set; }
+        }
+
+        public MainWindow() {
+            InitializeComponent();
+
+            MouseDown += (_, _) => FocusManager.SetFocusedElement(this, this);
+        }
+
+        public void GetAccountInfo() {
+            UserIcon.Source = new BitmapImage(new Uri(App.Account?.Thumb ?? "/Resources/PlexIcon.png"));
+            UserNameText.Text = App.Account?.Title ?? App.Account?.Username ?? "Name";
+            UserServerComboBox.ItemsSource = App.ServerContainer?.Servers;
+            UserServerComboBox.SelectedIndex = 0;
+
+            LoadingImage.Visibility = Visibility.Collapsed;
+        }
+
+        public async Task StartPolling() {
+            while (true) {
+                SessionMetadata? currentSession = await GetCurrentSession();
+                if (currentSession != null) {
+                    if (JsonConvert.SerializeObject(currentSession) != JsonConvert.SerializeObject(Session)) {
+                        if (currentSession != null) {
+                            SystemSounds.Exclamation.Play();
+                            SetPresence(await BuildPresence(currentSession));
+                        }
+                        Session = currentSession;
+                    }
+                    await Task.Delay(TimeSpan.FromSeconds(2));
+                }
+                else await Task.Delay(TimeSpan.FromSeconds(5));
+            }
+        }
+
+        private async Task<SessionMetadata?> GetCurrentSession() {
+            AccountServer? selected = UserServerComboBox.SelectedItem as AccountServer;
+            SessionContainer sessions = await App.ServerClient.GetSessionsAsync(App.Token, selected?.Uri.ToString());
+            return sessions.Metadata.FirstOrDefault(session => session.Type == "track" && session.User.Title == App.Account?.Username);
+        }
+
+        private async Task<PresenceData> BuildPresence(SessionMetadata session) {
+            return new PresenceData() {
+                Title = session.Title,
+                Artist = $"by {session.GrandparentTitle}",
+                Album = session.ParentTitle,
+                ArtLink = await GetThumbnail(session),
+                State = session.Player.State,
+                TimeOffset = (int)session.ViewOffset
+            };
+        }
+
+        private void SetPresence(PresenceData data) {
+            if (data.State == "playing") {
+                App.DiscordClient.SetPresence(new RichPresence() {
+                    Details = data.Title,
+                    State = data.Artist,
+                    Timestamps = new(DateTime.UtcNow.AddMilliseconds(-(double)data.TimeOffset)),
+                    Assets = new() {
+                        LargeImageKey = data.ArtLink,
+                        LargeImageText = data.Album
+                    }
+                });
+            }
+            else {
+                App.DiscordClient.SetPresence(new RichPresence() {
+                    Details = data.Title,
+                    State = data.Artist,
+                    Assets = new() {
+                        LargeImageKey = data.ArtLink,
+                        LargeImageText = data.Album,
+                        SmallImageKey = "paused2",
+                        SmallImageText = "Paused",
+                    }
+                });
+            }
+        }
+
+        private async Task<string> GetThumbnail(SessionMetadata session) {
+            Dictionary<string, string> thumbnails;
+            string thumbnailLink;
+
+            if (File.Exists("cache.json"))
+                thumbnails = JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText("cache.json"));
+            else
+                thumbnails = new();
+
+
+            if (thumbnails.TryGetValue(session.Thumb, out string value)) {
+                thumbnailLink = value;
+            }
+            else {
+                thumbnailLink = await UploadImage(session);
+                thumbnails.Add(session.Thumb, thumbnailLink);
+            }
+
+            File.WriteAllText("cache.json", JsonConvert.SerializeObject(thumbnails));
+
+            return thumbnailLink;
+        }
+
+        private async Task<string> UploadImage(SessionMetadata session) {
+            AccountServer? selected = UserServerComboBox.SelectedItem as AccountServer;
+            int artRes = 256;
+            string url = $"{selected?.Uri.ToString()}photo/:/transcode?width={artRes}&height={artRes}&minSize=1&upscale=1&format=png&url={session.Thumb}&X-Plex-Token={App.Token}";
+
+            var client = new HttpClient();
+            var response = await client.GetAsync(url);
+            var stream = await response.Content.ReadAsStreamAsync();
+
+            byte[] imageData = new byte[stream.Length];
+            stream.Read(imageData, 0, imageData.Length);
+            stream.Close();
+
+            const int MAX_URI_LENGTH = 32766;
+            string base64img = System.Convert.ToBase64String(imageData);
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < base64img.Length; i += MAX_URI_LENGTH) {
+                sb.Append(Uri.EscapeDataString(base64img.Substring(i, Math.Min(MAX_URI_LENGTH, base64img.Length - i))));
+            }
+
+            string uploadRequestString = "image=" + sb.ToString() + "&key=6d207e02198a847aa98d0a2a901485a5";
+
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create("https://freeimage.host/api/1/upload");
+            webRequest.Method = "POST";
+            webRequest.ContentType = "application/x-www-form-urlencoded";
+
+            StreamWriter streamWriter = new StreamWriter(webRequest.GetRequestStream());
+            streamWriter.Write(uploadRequestString);
+            streamWriter.Close();
+
+            WebResponse response2 = webRequest.GetResponse();
+            System.IO.Stream responseStream = response2.GetResponseStream();
+            StreamReader responseReader = new StreamReader(responseStream);
+
+            return JsonConvert.DeserializeObject<dynamic>(responseReader.ReadToEnd()).image.url;
+        }
+    }
+}
