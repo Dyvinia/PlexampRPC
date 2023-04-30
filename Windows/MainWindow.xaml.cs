0d0830d97964ebe1b2fb565030c5e7a673d3bd1b
Dyvine57 <dulana57@gmail.com>
2023-04-30 15:51:04 -0400

    Use Byte Array directly instead of Stream

--- Windows/MainWindow.xaml.cs
+++ Windows/MainWindow.xaml.cs
@@ -234,16 +234,11 @@ namespace PlexampRPC {
             AccountServer? selected = UserServerComboBox.SelectedItem as AccountServer;
 
             HttpResponseMessage getResponse = await Client.GetAsync($"{selected?.Uri}photo/:/transcode?width={Config.Settings.ArtResolution}&height={Config.Settings.ArtResolution}&minSize=1&upscale=1&format=png&url={session.Thumb}&X-Plex-Token={App.Token}");
-            System.IO.Stream stream = await getResponse.Content.ReadAsStreamAsync();
-
-            byte[] imageData = new byte[stream.Length];
-            await stream.ReadAsync(imageData);
-            stream.Close();
 
             HttpResponseMessage sendResponse = await Client.SendAsync(new() {
                 Method = HttpMethod.Post,
                 RequestUri = new("https://freeimage.host/api/1/upload"),
-                Content = new StringContent($"image={Uri.EscapeDataString(Convert.ToBase64String(imageData))}&key=6d207e02198a847aa98d0a2a901485a5", Encoding.UTF8, "application/x-www-form-urlencoded")
+                Content = new StringContent($"image={Uri.EscapeDataString(Convert.ToBase64String(await getResponse.Content.ReadAsByteArrayAsync()))}&key=6d207e02198a847aa98d0a2a901485a5", Encoding.UTF8, "application/x-www-form-urlencoded")
             });
             sendResponse.EnsureSuccessStatusCode();
             return JsonConvert.DeserializeObject<dynamic>(await sendResponse.Content.ReadAsStringAsync())!.image.url;
